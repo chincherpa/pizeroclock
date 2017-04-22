271b6964f5fac94d75c062555000240c23e8aa78
chincherpa <accounts@mail.de>
2017-04-22 21:13:51 +0200

    Pin changed GPIO18 -> GPIO20

--- button.py
+++ button.py
@@ -3,14 +3,13 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-# this is a comment
+GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 count = 1
 
 while count < 5:
-	input_state = GPIO.input(18)
-	if input_state == False:
-		print "gedrueckt Nr. " + str(count)
-		count += 1
-		time.sleep(0.2)
+    input_state = GPIO.input(20)
+    if not input_state:
+        print "Button pressed #" + str(count)
+        count += 1
+        time.sleep(0.2)
