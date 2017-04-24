ccaa15bc49eaba02858e7c300e942da37a2899bf
chincherpa <accounts@mail.de>
2017-04-24 13:53:39 +0200

    Update button.py

--- button.py
+++ button.py
@@ -1,15 +1,17 @@
 import RPi.GPIO as GPIO
 import time
 
+ipin = 20
+
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(ipin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 count = 1
 
 while count < 5:
-    input_state = GPIO.input(20)
+    input_state = GPIO.input(ipin)
     if not input_state:
         print "Button pressed #" + str(count)
         count += 1
-        time.sleep(2)
+        time.sleep(1)
